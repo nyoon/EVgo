99293ea00ae43fd9784c4bae92fdc89acc012f4c
Nicholas Yoon <nyoon90@gmail.com>
2020-03-04 13:35:22 -0800

    Initial xcode commit

---
+++ EVgo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EVgo
+//
+//  Created by Nicholas Yoon on 3/4/20.
+//  Copyright © 2020 Nicholas Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
