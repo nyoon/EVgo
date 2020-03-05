57abeb41c1041a83c9a1d76012e12edcf0addc00
Nicholas Yoon <nyoon90@gmail.com>
2020-03-05 11:26:44 -0800

    Adding the frameworks

---
+++ EVgoTests/LaunchesPastPresenterSpec.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchesPastPresenterSpec.swift
+//  EVgoTests
+//
+//  Created by Nicholas Yoon on 3/5/20.
+//  Copyright © 2020 Nicholas Yoon. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import EVgo
+
+class LaunchesPastPresenterSpec: QuickSpec {
+    
+}
